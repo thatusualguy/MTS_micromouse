b3158ddb6a6aadb9b2060b843045ce36628b02cb
Maksim Rybakov <m.p.rybakov@gmail.com>
2024-10-22 19:47:37 +0300

    fix

--- MouseCommand.py
+++ MouseCommand.py
@@ -11,8 +11,10 @@ class MouseCommands(object):
 
 
     @staticmethod
-    def move(distance):
-        pass
+    def move(left, left_time, right, right_time):
+        data = {"id":robot_id, "l": int(left), "r":int(right), "l_rime":left_time, "r_rime":right_time}
+        url = baseUrl + '/' + "motor"
+        print(requests.put(url, json = data).text)
 
 
     @staticmethod
@@ -41,13 +43,37 @@ class MouseCommands(object):
 
     @staticmethod
     def turn_left_90():
-        print("left")
-        angle = 90
-        target =( MouseCommands.get_yaw_now() - 90) % 360
-        data = {"id":robot_id, "direction": "left", "len": int(angle+2)}
-        url = baseUrl + '/' + "move"
-        requests.put(url, json = data)
-        print( MouseCommands.get_yaw_now())
+        # print("left")
+        # angle = 90
+        # target =( MouseCommands.get_yaw_now() - 90) % 360
+        # data = {"id":robot_id, "direction": "left", "len": int(angle+2)}
+        # url = baseUrl + '/' + "move"
+        # requests.put(url, json = data)
+        # print( MouseCommands.get_yaw_now())
+
+        start_angle = MouseCommands.get_yaw_now()
+        current_angle = MouseCommands.get_yaw_now()
+        end_angle = start_angle + 270
+        while abs(current_angle - end_angle)>2:
+            diff = current_angle - end_angle
+            print(diff)
+            # magnitude = 255 * abs(diff)/90 * 2
+            magnitude = 255
+            if abs(diff) < 10:
+                magnitude = abs(diff)*2
+
+            if diff > 0:
+                # left
+                print("left ", magnitude)
+                MouseCommands.move(-magnitude, 200, magnitude, 200)
+                pass
+            elif diff < 0:
+                #right
+                print("right ", magnitude)
+                MouseCommands.move(magnitude, 200, -magnitude, 200)
+                pass
+            time.sleep(0.2)
+            current_angle = MouseCommands.get_yaw_now()
 
     @staticmethod
     def sensors():
