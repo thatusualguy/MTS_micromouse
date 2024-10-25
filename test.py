d1058bf4daea1b4c3a11aaeda31c9ce730669719
Maksim Rybakov <m.p.rybakov@gmail.com>
2024-10-25 12:28:51 +0300

    a

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
-from MouseCommand import MouseCommands
+import requests
 
+from MouseCommand import MouseCommands
+from shared import real_baseUrl, real_robotId
 
 if __name__ == "__main__":
-    print(MouseCommands.turn_right_90())
+    left = 120
+    right = 120
+    left_time = 500
+    right_time = 500
+    data = {"id":real_robotId, "l": int(left), "r":int(right), "l_rime":left_time, "r_rime":right_time}
+    url = real_baseUrl + '/' + "motor"
+    print(requests.put(url, json = data).text)
