b2c0d5bc5e444d8238a58f9240902ed78d359c5b
Maksim Rybakov <m.p.rybakov@gmail.com>
2024-11-05 17:25:05 +0300

    new features

--- run_by_history.py
+++ run_by_history.py
@@ -6,8 +6,19 @@ def run_by_history():
     logging.info('start run by history')
     robot = AA(isRightHand=True)
     filename = input('get filename of history: ')
+
+    if filename == "":
+        filename = "test_labirint.json"
+
     robot.load_history(filename)
-    logging.info('f{filename} is readed, lets start')
+    logging.info(f'{filename} is readed, lets start')
+    skip = input('How much to skip? ')
+
+    if skip == "":
+        skip = 0
+    else:
+        skip = int(skip)
+
     start = input('Start?')
     logging.info('run by history')
     robot.run_by_history()
